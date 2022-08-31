3794d6122abe443b068519d26ad73e75f0193e1a
dcccastro <diego.ccastro@hotmail.com>
2022-08-31 01:48:23 -0300

    added new app_config file

--- app_config.py
+++ app_config.py
@@ -1,3 +1,3 @@
 DEBUG = True
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:ytkb99876ope@localhost:5432/postgres'
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@localhost:5432/postgres'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
